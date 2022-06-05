a294486ff87bd7382daad157c276f3438720c7ed
915marco <915marco@gmail.com>
2022-06-05 14:54:26 -0500

    add replies complete

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,23 @@
-const router = require('express').Router();
+// const router = require('express').Router();
+// // const pizzaRoutes = require('./pizza-routes');
+// const commentRoutes = require('./comment-routes');
 // const pizzaRoutes = require('./pizza-routes');
+
+// router.use('/comments', commentRoutes);
+// // router.use('/pizzas', pizzaRoutes);
+
+// // add prefix of `/pizzas` to routes created in `pizzas-routes.js`
+// router.use('/pizzas', pizzaRoutes);
+
+// module.exports = router;
+
+
+
+const router = require('express').Router();
 const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
 
 router.use('/comments', commentRoutes);
-// router.use('/pizzas', pizzaRoutes);
-
-// add prefix of `/pizzas` to routes created in `pizzas-routes.js`
 router.use('/pizzas', pizzaRoutes);
 
 module.exports = router;
